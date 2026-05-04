cacdc9fed046d11fa184aaf7bb9d6e49b8917b70
Sufiyan Shahid Professional Programmer <sufiyanshahid144@gmail.com>
2026-05-04 14:28:41 +0500

    Initial commit

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
 
-// const colors = [
-//     "AliceBlue", "AntiqueWhite", "Aquamarine", "BurlyWood",
-//     "Chartreuse", "CornflowerBlue", "RoyalBlue", "DarkSlateBlue",
-//     "DarkSeaGreen", "DodgerBlue", "DarkCyan", "ForestGreen",
-//     "LightBlue", "PaleTurquoise", "LightSteelBlue", "LightSeaGreen",
-//     "LimeGreen", "MediumSlateBlue", "CadetBlue", "SkyBlue"
-// ];
-// const bodyTag = document.getElementById('body-bg');
-// const displayColor = document.getElementById('color-name');
-// const btn = document.getElementById('change-btn');
+const colors = [
+    "AliceBlue", "AntiqueWhite", "Aquamarine", "BurlyWood",
+    "Chartreuse", "CornflowerBlue", "RoyalBlue", "DarkSlateBlue",
+    "DarkSeaGreen", "DodgerBlue", "DarkCyan", "ForestGreen",
+    "LightBlue", "PaleTurquoise", "LightSteelBlue", "LightSeaGreen",
+    "LimeGreen", "MediumSlateBlue", "CadetBlue", "SkyBlue"
+];
+const bodyTag = document.getElementById('body-bg');
+const displayColor = document.getElementById('color-name');
+const btn = document.getElementById('change-btn');
 
-// function changeBackgroundColor() {
+function changeBackgroundColor() {
 
-//     const randomIndex = Math.floor(Math.random() * colors.length);
+    const randomIndex = Math.floor(Math.random() * colors.length);
 
-//     const bgcolor = colors[randomIndex];
+    const bgcolor = colors[randomIndex];
 
-//     bodyTag.style.backgroundColor = bgcolor;
+    bodyTag.style.backgroundColor = bgcolor;
 
-//     displayColor.innerHTML = bgcolor;
+    displayColor.innerHTML = bgcolor;
 
-//     const darkColors = ["DarkSlateBlue", "DarkCyan", "ForestGreen", "RoyalBlue", "DarkSeaGreen", "LightSeaGreen"];
+    const darkColors = ["DarkSlateBlue", "DarkCyan", "ForestGreen", "RoyalBlue", "DarkSeaGreen", "LightSeaGreen"];
 
-//     if (darkColors.includes(bgcolor)) {
-//         displayColor.style.color = "white";
-//     } else {
-//         displayColor.style.color = "black";
-//     }
-// }
+    if (darkColors.includes(bgcolor)) {
+        displayColor.style.color = "white";
+    } else {
+        displayColor.style.color = "black";
+    }
+}
